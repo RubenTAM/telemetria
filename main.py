1e26cc3f34f79b5f81c4462da472b60edfebc00c
Ruben TAM <rubenr@tecnoall.com>
2026-02-26 10:03:54 -0800

    Fix CORS

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import ssl
 import threading
 import time
 import paho.mqtt.client as mqtt
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
